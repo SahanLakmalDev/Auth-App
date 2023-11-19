2ec12fa728f0ac3f099fbfb336887c1e681b45a7
Sahan Lakmal <sahanloser@gmail.com>
2023-11-19 06:48:43 +0530

    Create sign in api route

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
+import jwt  from "jsonwebtoken";
 
 
 export const signup = async (req, res, next) => {
@@ -21,6 +23,26 @@ export const signup = async (req, res, next) => {
         // console.error("Error saving book:", error);
         next(error);
     }
-    
-
 };
+
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body;
+    try {
+        const validUser = await User.findOne({email});
+        if(!validUser){
+            return next(errorHandler(404, 'User Not Found'))
+        }
+        const isValidPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!isValidPassword) {
+            return next(errorHandler(403, 'Wrong credentials'));
+        }
+        const expireDate = new Date(Date.now() + 3600000);
+        let token = jwt.sign({_id: validUser._id}, process.env.JWT_SECRET);
+        const {password:hashedPassword, ...rest} = validUser._doc;
+        res.cookie('access token', token, {httpOnly: true, expires:expireDate}).status(200).json(rest);
+
+
+    }catch (error) {
+        next(error);
+    }
+}
