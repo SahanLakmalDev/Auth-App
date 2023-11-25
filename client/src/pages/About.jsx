82a64019da32bf9509358ea6d3bc71560c938f33
Sahan Lakmal <sahanloser@gmail.com>
2023-11-25 22:20:13 +0530

    Completed home and about pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,9 +2,30 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
+    <div className='px-4 py-14 max-w-3xl mx-auto'>
+    <h1 className='text-4xl text-center font-bold mb-4 text-slate-800'>
       About
+    </h1>
+    <div className='mt-10 bg-slate-100 p-8 rounded shadow-md'>
+      <p className='mb-4 bg-white p-8 rounded shadow-md text-slate-700 animate__animated animate__fadeIn border-l-4 border-r-4 border-x-indigo-500'>
+        This is a MERN (MongoDB, Express, React, Node.js) stack application with
+        authentication. It allows users to sign up, log in, and log out, and
+        provides access to protected routes only for authenticated users.
+      </p>
+      <p className='mb-4 bg-white p-8 rounded shadow-md text-slate-700 animate__animated animate__fadeIn border-l-4 border-r-4 border-x-indigo-500'>
+        The front-end of the application is built with React and uses React
+        Router for client-side routing. The back-end is built with Node.js and
+        Express, and uses MongoDB as the database. Authentication is implemented
+        using JSON Web Tokens (JWT).
+      </p>
+      <p className='mb-4 bg-white p-8 rounded shadow-md text-slate-700 animate__animated animate__fadeIn border-l-4 border-r-4 border-x-indigo-500'>
+        This application is intended as a starting point for building full-stack
+        web applications with authentication using the MERN stack. Feel free to
+        use it as a template for your own projects!
+      </p>
+     
     </div>
+  </div>
   )
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React from 'react';
+import 'animate.css';
+
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className='px-4 py-14 max-w-4xl mx-auto'>
+    <h1 className='text-4xl text-center font-bold mb-4 text-slate-800'>
+      Welcome to My Auth App!
+    </h1>
+    <div className='mt-10 bg-slate-100 p-8 rounded shadow-md'>
+      <p className='mb-4 bg-white p-8 rounded shadow-md text-slate-700 animate__animated animate__fadeIn border-l-4 border-r-4 border-x-indigo-500'>
+        Explore the capabilities of this robust full-stack web application crafted with the powerful MERN (MongoDB, Express, React, Node.js) stack. Seamlessly integrating front-end and back-end technologies, this app showcases comprehensive authentication features for a secure and user-friendly experience.
+      </p>
+      <p className='mb-4 bg-white p-8 rounded shadow-md text-slate-700 animate__animated animate__fadeIn border-l-4 border-r-4 border-x-indigo-500'>
+        The front-end, developed with React, employs React Router for smooth client-side routing, ensuring a seamless and responsive user interface. On the server side, Node.js and Express handle the back-end operations, while MongoDB serves as the efficient and scalable database solution.
+      </p>
+      <p className='mb-4 bg-white p-8 rounded shadow-md text-slate-700 animate__animated animate__fadeIn border-l-4 border-r-4 border-x-indigo-500'>
+        Leveraging the power of JSON Web Tokens (JWT), our authentication implementation ensures a high level of security. Users can easily sign up, log in, and log out, with access restricted to protected routes exclusively for authenticated users.
+      </p>
+      <p className='mb-4 bg-white p-8 rounded shadow-md text-slate-700 animate__animated animate__fadeIn border-l-4 border-r-4 border-x-indigo-500'>
+        Whether you are a seasoned developer or just starting your journey, this application stands as an insightful template for building your own full-stack web projects. Take advantage of its structure and features to expedite your development process and create robust, scalable, and secure applications.
+      </p>
     </div>
+  </div>
   )
 }
 
